498d8ef7fc1a4bf9d69ad2ecef6c91b89defc5b5
Hihiz <public@mail.ru>
2026-07-22 02:56:42 +0300

    Добавить инициализацию функций

---
+++ js/main.js
@@ -0,0 +1,9 @@
+import { initHamburger } from "./hamburger.js";
+import { initAccordion } from "./accordion.js";
+import { initSliderTestimonial } from "./slider-testimonial.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  initHamburger();
+  initAccordion();
+  initSliderTestimonial();
+});
